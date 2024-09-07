aade55235b9e287bc4c2a0d14361dc4a20fdddb1
aannddrree <aannddrree.silva@gmail.com>
2024-09-07 18:19:44 -0300

    v6

--- src/main/java/org/apiuniara/controllers/AlunoController.java
+++ src/main/java/org/apiuniara/controllers/AlunoController.java
@@ -3,10 +3,8 @@ package org.apiuniara.controllers;
 import org.apiuniara.models.Aluno;
 import org.apiuniara.services.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 @RestController
 public class AlunoController {
@@ -16,11 +14,13 @@ public class AlunoController {
     @Autowired
     private AlunoService alunoService;
 
+    @CrossOrigin
     @PostMapping(API_ENDPOINT)
     public Aluno add(@RequestBody Aluno aluno){
         return alunoService.add(aluno);
     }
 
+    @CrossOrigin
     @GetMapping(API_ENDPOINT)
     public List<Aluno> get(){
         return alunoService.get();
